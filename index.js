4e6046234093b01397229c3075df9d0f421cefb7
Miguel 'Mikasonwar' Pereira <mikasisto2@gmail.com>
2018-05-24 11:31:35 +0100

    ajustes

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-// $(document).ready(function(){
-//   $("#steam").click(function(){
-//   window.location.href = "https://steamcommunity.com/id/mikasonwar";
-//   });
-// });
+$(document).ready(function(){
+
+  $("#cvideojogos").hide();
+  $("#ceducacao").hide();
+  $("#cprojetos").hide();
+  $("#cstaffatual").hide();
+  $("#cstaffantiga").hide();
+  $("#chabilidades").hide();
+
+  $("#videojogos").on("click",function(){
+    $("#cvideojogos").slideToggle();
+  });
+  $("#educacao").on("click",function(){
+    $("#ceducacao").slideToggle();
+  });
+  $("#projetos").on("click",function(){
+    $("#cprojetos").slideToggle();
+  });
+  $("#staffatual").on("click",function(){
+    $("#cstaffatual").slideToggle();
+  });
+  $("#staffantiga").on("click",function(){
+    $("#cstaffantiga").slideToggle();
+  });
+  $("#habilidades").on("click",function(){
+    $("#chabilidades").slideToggle();
+  });
+
+  $(".csshidden").on("click", function(){
+    $(".Jquery").slideToggle();
+  });
+});
+
