09a29153c1a737e32f543f77d3d84dbd47110777
Miguel 'Mikasonwar' Pereira <mikasisto2@gmail.com>
2018-05-25 15:24:13 +0100

    Avancos!

--- clicker/clicker.js
+++ clicker/clicker.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+var teste = 0;
+$("#numero").html(teste+" Cães");
 
+$("#clicker").on("click", function(){
+  teste+=1;
+  $("#numero").html(teste+" Cães");
+});
 
 });
